266f5563dc6f539bebe356cf3bd7f6132933ae10
eliasall3x <elliasallex@gmail.com>
2021-03-15 18:39:17 -0300

    feat: VScode Debug configured

--- rentalx/src/server.ts
+++ rentalx/src/server.ts
@@ -6,6 +6,12 @@ app.use(express.json());
 
 app.get("/", (_request, response) => response.json({ ok: true }));
 
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ course: name });
+});
+
 app.listen(3333, () => console.log("Server is running on port 3333"));
 
 export default app;
