dc08d0659cc24a3e1a702d0bf43467a6a52c4132
eliasall3x <elliasallex@gmail.com>
2021-03-28 17:47:43 -0300

    feat: LSP

--- rentalx/src/services/CreateCategoryService.ts
+++ rentalx/src/services/CreateCategoryService.ts
@@ -1,4 +1,4 @@
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
 
 interface IRequest {
   name: string;
@@ -10,7 +10,7 @@ interface IRequest {
  * [x] - Accesar o repositories
  */
 class CreateCategoryService {
-  constructor(private categoriesRepository: CategoriesRepository) {}
+  constructor(private categoriesRepository: ICategoriesRepository) {}
 
   execute({ name, description }: IRequest): void {
     const CategoryAlreadyExists = this.categoriesRepository.findByName(name);
