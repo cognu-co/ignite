2101a63a32701f7fa531824c364ad27060f82523
eliasall3x <elliasallex@gmail.com>
2021-04-09 16:27:02 -0300

    feat: refatoração docker com TypeORM

--- rentx/src/database/index.ts
+++ rentx/src/database/index.ts
@@ -1,3 +1,13 @@
-import { createConnection } from "typeorm";
+import { createConnection, getConnectionOptions } from "typeorm";
 
-createConnection();
+interface IOptions {
+  host: string;
+}
+
+getConnectionOptions().then((options) => {
+  const newOptions = options as IOptions;
+  newOptions.host = "database_ignite"; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+  createConnection({
+    ...options,
+  });
+});
