9880f260d05cf64ac8b28234977e0de0a0dcada0
eliasall3x <elliasallex@gmail.com>
2021-04-28 01:11:44 -0300

    feat: user upload avatar

--- rentx/src/routes/users.routes.ts
+++ rentx/src/routes/users.routes.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import multer from "multer";
 
+import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 const usersRoutes = Router();
 
-const upload = multer({
-  dest: "./avatar",
-});
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
 
 const createUserController = new CreateUserController();
 usersRoutes.post("/", createUserController.handle);
@@ -16,7 +16,8 @@ usersRoutes.post("/", createUserController.handle);
 const updateUserAvatarController = new UpdateUserAvatarController();
 usersRoutes.patch(
   "/avatar",
-  upload.single("avatar_file"),
+  ensureAuthenticated,
+  uploadAvatar.single("avatar"),
   updateUserAvatarController.handle
 );
 
