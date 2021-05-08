7b10b716123aa12c45af55b81d67d8abda4402b3
eliasall3x <elliasallex@gmail.com>
2021-05-07 23:43:01 -0300

    feat: categories second test created

--- rentx/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ rentx/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from "../../../../errors/AppError";
 import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
@@ -24,8 +25,18 @@ describe("Create Category", () => {
       category.name
     );
 
-    console.log(categoryCreated);
-
     expect(categoryCreated).toHaveProperty("id");
+  });
+
+  it("should not be able to create a new category with duplicate name", async () => {
+    expect(async () => {
+      const category = {
+        name: "new category",
+        description: "description for new category",
+      };
+
+      await createCategoryUseCase.execute(category);
+      await createCategoryUseCase.execute(category);
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
