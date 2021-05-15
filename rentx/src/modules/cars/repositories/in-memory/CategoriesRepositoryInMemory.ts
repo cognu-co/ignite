52aefa8f0479f5ca43de528175b11089270863c6
eliasall3x <elliasallex@gmail.com>
2021-05-15 20:35:03 -0300

    feat: Migrations folder redirected

--- rentx/src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
+++ rentx/src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -1,4 +1,5 @@
-import { Category } from "../../infra/typeorm/entities/Category";
+import { Category } from "@modules/cars/infra/typeorm/entities/Category";
+
 import {
   ICategoriesRepository,
   ICreateCategoryDTO,
