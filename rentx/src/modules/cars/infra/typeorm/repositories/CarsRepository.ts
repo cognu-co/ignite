4b499c9627140d6c1ccedf6b7f262dfc2efc71d4
eliasall3x <elliasallex@gmail.com>
2021-05-16 20:49:16 -0300

    feat: Car repo init

---
+++ rentx/src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -0,0 +1,43 @@
+import { getRepository } from "typeorm";
+import { Repository } from "typeorm/repository/Repository";
+
+import { ICreateCarDTO } from "../../../dtos/ICreateCarDTO";
+import { ICarsRepository } from "../../../repositories/ICarsRepository";
+import { Car } from "../entities/Car";
+
+class CarsRepository implements ICarsRepository {
+  private repository: Repository<Car>;
+
+  constructor() {
+    this.repository = getRepository(Car);
+  }
+
+  async create({
+    name,
+    description,
+    daily_rate,
+    license_plate,
+    fine_amount,
+    brand,
+    category_id,
+  }: ICreateCarDTO): Promise<Car> {
+    return this.repository.create({
+      name,
+      description,
+      daily_rate,
+      license_plate,
+      fine_amount,
+      brand,
+      category_id,
+    });
+  }
+
+  findByLicensePlate(license_plate: string): Promise<Car> {
+    return this.repository.findOne({
+      where: {
+        license_plate,
+      },
+    });
+  }
+}
+export { CarsRepository };
