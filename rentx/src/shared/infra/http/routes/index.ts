f758348cb49614bc05608293be988263c807709e
eliasall3x <elliasallex@gmail.com>
2021-05-16 21:12:36 -0300

    feat: added cars controller and router

--- rentx/src/shared/infra/http/routes/index.ts
+++ rentx/src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { authenticateRoutes } from "./authenticate.routes";
+import { carsRoutes } from "./cars.routes";
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./specifications.routes";
 import { usersRoutes } from "./users.routes";
@@ -10,6 +11,7 @@ const routes = Router();
 routes.use("/users", usersRoutes);
 routes.use("/categories", categoriesRoutes);
 routes.use("/specifications", specificationsRoutes);
+routes.use("/cars", carsRoutes);
 routes.use(authenticateRoutes);
 
 export { routes };
