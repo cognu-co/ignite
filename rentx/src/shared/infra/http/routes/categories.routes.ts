b5091153d55335134f5df70225009947f2eee1ff
eliasall3x <elliasallex@gmail.com>
2021-05-17 22:10:59 -0300

    feat: admin permissions requested in Routes

--- rentx/src/shared/infra/http/routes/categories.routes.ts
+++ rentx/src/shared/infra/http/routes/categories.routes.ts
@@ -4,6 +4,8 @@ import multer from "multer";
 import { CreateCategoryController } from "../../../../modules/cars/useCases/createCategory/CreateCategoryController";
 import { ImportCategoryController } from "../../../../modules/cars/useCases/importCategory/importCategoryController";
 import { ListCategoriesController } from "../../../../modules/cars/useCases/listCaregories/ListCategoriesController";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { ensureIsAdmin } from "../middlewares/ensureIsAdmin";
 
 const categoriesRoutes = Router();
 const upload = multer({
@@ -11,7 +13,12 @@ const upload = multer({
 });
 
 const createCategoryController = new CreateCategoryController();
-categoriesRoutes.post("/", createCategoryController.handle);
+categoriesRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureIsAdmin,
+  createCategoryController.handle
+);
 
 const listCategoriesController = new ListCategoriesController();
 categoriesRoutes.get("/", listCategoriesController.handle);
@@ -20,6 +27,8 @@ const importCategoryController = new ImportCategoryController();
 categoriesRoutes.post(
   "/import",
   upload.single("file"),
+  ensureAuthenticated,
+  ensureIsAdmin,
   importCategoryController.handle
 );
 
--- rentx/src/shared/infra/http/routes/specifications.routes.ts
+++ rentx/src/shared/infra/http/routes/specifications.routes.ts
@@ -3,12 +3,18 @@ import { Router } from "express";
 import { CreateSpecificationController } from "../../../../modules/cars/useCases/createSpecification/CreateSpecificationController";
 import { ListSpecificationsController } from "../../../../modules/cars/useCases/listSpecifications/ListSpecificationsController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { ensureIsAdmin } from "../middlewares/ensureIsAdmin";
 
 const specificationsRoutes = Router();
 
-specificationsRoutes.use(ensureAuthenticated);
 const createSpecificationController = new CreateSpecificationController();
-specificationsRoutes.post("/", createSpecificationController.handle);
+
+specificationsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureIsAdmin,
+  createSpecificationController.handle
+);
 
 const listSpecificationsController = new ListSpecificationsController();
 specificationsRoutes.get("/", listSpecificationsController.handle);
