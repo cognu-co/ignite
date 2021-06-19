cb040ca5a304553e7c7efbcd4089c138b6249edb
eliasall3x <elliasallex@gmail.com>
2021-06-18 22:55:14 -0300

    feat: Rental creation - controller

--- rentx/src/modules/rentals/useCases/createRental/CreateRentalController.ts
+++ rentx/src/modules/rentals/useCases/createRental/CreateRentalController.ts
@@ -1,2 +1,22 @@
-class CreateRentalController {}
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { CreateRentalUseCase } from "./CreateRentalUseCase";
+
+class CreateRentalController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const createRentalUseCase = container.resolve(CreateRentalUseCase);
+
+    const { car_id, expected_return_date } = request.body;
+    const { id: user_id } = request.user;
+
+    const rental = createRentalUseCase.execute({
+      user_id,
+      car_id,
+      expected_return_date,
+    });
+
+    return response.status(201).json(rental);
+  }
+}
 export { CreateRentalController };
