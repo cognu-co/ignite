7381a83abcfb22b4e21f1d6e1a6f9369b4341317
eliasall3x <elliasallex@gmail.com>
2021-06-18 22:59:14 -0300

    feat: Rental access point

--- rentx/src/shared/infra/http/routes/index.ts
+++ rentx/src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { authenticateRoutes } from "./authenticate.routes";
 import { carsRoutes } from "./cars.routes";
 import { categoriesRoutes } from "./categories.routes";
+import { rentalRoutes } from "./rental.routes";
 import { specificationsRoutes } from "./specifications.routes";
 import { usersRoutes } from "./users.routes";
 
@@ -12,6 +13,7 @@ routes.use("/users", usersRoutes);
 routes.use("/categories", categoriesRoutes);
 routes.use("/specifications", specificationsRoutes);
 routes.use("/cars", carsRoutes);
+routes.use("/rentals", rentalRoutes);
 routes.use(authenticateRoutes);
 
 export { routes };
