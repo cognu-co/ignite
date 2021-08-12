b0d010ffe421016a013a3c2bf05574b0af918aa5
elias alexandre <elliasallex@gmail.com>
2021-08-12 00:33:42 -0300

    feat: List rentals from user with Car relation

--- rentx/src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ rentx/src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -47,7 +47,12 @@ class RentalsRepository implements IRentalsRepository {
   }
 
   async findByUser(user_id: string): Promise<Rental[]> {
-    const rentalsByUser = await this.repository.find({ user_id });
+    const rentalsByUser = await this.repository.find({
+      where: {
+        user_id,
+      },
+      relations: ["car"],
+    });
 
     return rentalsByUser;
   }
