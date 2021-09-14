9d160c74a6a4a2180c625d9d3a9b8b8da44c4e1c
elias alexandre <elliasallex@gmail.com>
2021-09-13 23:26:52 -0300

    feat: refresh token secret changes

--- rentx/src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ rentx/src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,28 +1,30 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
-import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import auth from "@config/auth";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 import { AppError } from "@shared/errors/AppError";
 
 interface IPayload {
   sub: string;
 }
 
+/**
+ * has token?
+ *
+ * has two parts?
+ *
+ * form?: Bearer <Token>
+ *   - ex: Bearer 20935g3249875g34v578945g934875
+ */
 export async function ensureAuthenticated(
   request: Request,
   response: Response,
   next: NextFunction
 ): Promise<void> {
-  /**
-   * has token?
-   *
-   * has two parts?
-   *
-   * form?: Bearer <Token>
-   *   - ex: Bearer 20935g3249875g34v578945g934875
-   */
-
   const authHeader = request.headers.authorization;
+
+  const usersTokensRepository = new UsersTokensRepository();
 
   if (!authHeader) {
     throw new AppError("Token missing", 401);
@@ -43,12 +45,13 @@ export async function ensureAuthenticated(
   try {
     const { sub: user_id } = verify(
       token,
-      "5d1e38bc9ff246383c727690ec1888ab"
+      auth.secret_refresh_token
     ) as IPayload;
 
-    const usersRepository = new UsersRepository();
-
-    const user = usersRepository.findById(user_id);
+    const user = usersTokensRepository.findByUserIdAndRefreshToken(
+      user_id,
+      token
+    );
     if (!user) {
       throw new AppError("User not found", 401);
     }
