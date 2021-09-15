0ad12c1c6aba0537067e57f0afaef32e2bafe02c
elias alexandre <elliasallex@gmail.com>
2021-09-14 22:36:40 -0300

    feat: Ethereal Mail provider singleton

--- rentx/src/shared/container/index.ts
+++ rentx/src/shared/container/index.ts
@@ -1,6 +1,7 @@
 import { container } from "tsyringe";
 
 import "@shared/container/providers";
+import "@shared/container/providers/MailProvider";
 
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
