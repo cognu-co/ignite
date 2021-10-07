1f4b4bb3292aaced043677fe696ab5f1b6aec356
elias alexandre <elliasallex@gmail.com>
2021-10-06 21:22:36 -0300

    feat: safe cars image on AWS Bucket

--- rentx/src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
+++ rentx/src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
 
 import { ICarImagesRepository } from "@modules/cars/repositories/ICarImagesRepository";
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
 
 interface IRequest {
   car_id: string;
@@ -11,13 +12,17 @@ interface IRequest {
 class UploadCarImagesUseCase {
   constructor(
     @inject("CarImagesRepository")
-    private carImagesRepository: ICarImagesRepository
+    private carImagesRepository: ICarImagesRepository,
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) {}
 
   async execute({ car_id, images_name }: IRequest): Promise<void> {
-    images_name.map(async (image) =>
-      this.carImagesRepository.create(car_id, image)
-    );
+    images_name.map(async (image) => {
+      await this.storageProvider.save(image, "cars");
+      await this.carImagesRepository.create(car_id, image);
+    });
   }
 }
+
 export { UploadCarImagesUseCase };
